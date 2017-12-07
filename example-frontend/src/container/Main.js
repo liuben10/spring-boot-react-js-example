d44b55af653d0199254d8eb2d1df778e8111a87a
liuben10 <liuben10@gmail.com>
2017-12-07 01:09:02 -0800

    Improve README.md as well as added state to the Main.js container

--- example-frontend/src/container/Main.js
+++ example-frontend/src/container/Main.js
@@ -4,9 +4,17 @@ import axios from 'axios';
 
 class Main extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {ponged: 'Not Ponged'}
+
+    this.ping = this.ping.bind(this);
+  }
+
   ping() {
     axios.get("http://localhost:8080/pong").then(res => {
       alert("Received Successful response from server!");
+      this.setState({ponged: 'Ponged! '});
     }, err => {
       alert("Server rejected response with: " + err);
     });
@@ -19,7 +27,10 @@ class Main extends Component {
           <h1 className="App-title">Ping</h1>
         </header>
         <p className="App-intro">
-          <button onClick={this.ping}>Ping!</button>
+          <div>
+            <button onClick={this.ping}>Ping!</button>
+            <div>Ponged: {this.state.ponged}</div>
+          </div>
         </p>
       </div>
     );
