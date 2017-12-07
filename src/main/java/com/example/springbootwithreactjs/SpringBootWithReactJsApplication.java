389dfd4901ccb04298a886cda9b8530690aff4ad
liuben10 <liuben10@gmail.com>
2017-12-06 23:16:39 -0800

    Empty Spring Boot project created from the spring boot initializer

---
+++ src/main/java/com/example/springbootwithreactjs/SpringBootWithReactJsApplication.java
@@ -0,0 +1,12 @@
+package com.example.springbootwithreactjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWithReactJsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWithReactJsApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/springbootwithreactjs/SpringBootWithReactJsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springbootwithreactjs;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootWithReactJsApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
